622928dace8e8d40341e8b99fa3c5ff33620bf09
Mike Bujold <mnbujold@ualberta.ca>
2013-11-27 11:44:26 -0700

    Paddle implemented - need to add boundary checking

--- README.md
+++ README.md
@@ -9,7 +9,8 @@ This is a version of the arcade classic Breakout! for the Arduino platform. (For
 This version was developed and runs on the Arduino Mega 2560. It uses the Adafruit ST7735 LCD module for output and a (10k) analog joystick for input. 
 
 Known Issues:
-- None at this time
+- Paddle boundaries not yet set
+- Paddle 'flickers'
 
 Special Thanks:  
 The great and powerful Woz - who inspired the idea for this implementation.
--- breakout.cpp
+++ breakout.cpp
@@ -11,22 +11,68 @@
 /* GLOBAL VARS */
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
-// Joystick Pins
-const int VERT = 0;
-const int HOR = 1;
-const int SEL = 9;
+// Paddle Size
+const int PADDLE_WIDTH = 25;
+const int PADDLE_HEIGHT = 5;
 
+// Joystick Pins
+const int VERT = 1;
+const int HOR = 0;
+const int SEL = 9;
+// Joystick Defs
+const int INCREMENT = 5;
+const int OFFSET = 30;
+int X_CENTRE;
+
+
+void drawPaddle(int position, int oldpos){
+  /* Draws the paddle at the bottom of the screen */
+  int vert = 10;
+ 
+  // Draw the paddle 
+  tft.fillRect(vert, oldpos, PADDLE_HEIGHT, PADDLE_WIDTH, ST7735_BLACK);
+  tft.fillRect(vert, position, PADDLE_HEIGHT, PADDLE_WIDTH, ST7735_WHITE);
+
+
+}
+
+int readJoystick(int position){
+  int joystick_x = analogRead(HOR);
+
+  if(joystick_x > X_CENTRE + OFFSET){
+    /* Move to the RIGHT by INCREMENT */
+    position += INCREMENT;
+  }
+  else if(joystick_x < X_CENTRE - OFFSET){
+    /* Move to the LEFT by INCREMENT */
+    position -= INCREMENT;
+  }
+
+  return(position);
+}
 
 void setup(){
   // Start serial communication for debugging
   Serial.begin(9600);
+
+  X_CENTRE = analogRead(HOR);
   
   tft.initR(INITR_REDTAB);
   pinMode(SEL, INPUT);
   digitalWrite(SEL, HIGH);
 
   tft.fillScreen(ST7735_BLACK);
+  int position = tft.height()/2;
+  int oldpos;
 
+  while(1){
+    oldpos = position;
+    position = readJoystick(position);
+    drawPaddle(position, oldpos);
+
+    delay(20);
+  }
+  
 }
 
 void loop(){}
