39902c3b383da5783e38a8efc323bd11e9c5b20b
Mike Bujold <mnbujold@ualberta.ca>
2013-12-01 13:32:27 -0700

    Added sounds

--- breakout.cpp
+++ breakout.cpp
@@ -27,6 +27,7 @@ const int INCREMENT = 5;
 const int OFFSET = 30;
 int X_CENTRE;
 
+const int speakerPin = 11;
 
 typedef struct Point {
   int x;
@@ -36,6 +37,20 @@ typedef struct Point {
 Point ball, oldball;
 int xdir, ydir;
 int paddlepos;
+
+
+void playTone(int period, int duration){
+  long elapsedTime = 0;
+  int halfPeriod = period/2;
+
+  while (elapsedTime < duration * 1000L){
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(halfPeriod);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(halfPeriod);
+    elapsedTime = elapsedTime + period;
+  }
+}
 
 void drawPaddle(int position, int oldpos){
   /* Draws the paddle at the bottom of the screen */
@@ -58,18 +73,21 @@ void checkBallPos(){
   if(ball.x > SCREEN_WIDTH){
     ball.x = SCREEN_WIDTH;
     xdir = -1;
+    playTone(500, 50);
     return;
   }
 
   if(ball.x < 0){
     ball.x = 0;
     xdir = 1;
+    playTone(500, 50);
     return;
   }
 
   if(ball.y > SCREEN_HEIGHT){
     ball.y = SCREEN_HEIGHT;
     ydir = -1;
+    playTone(500, 50);
     return;
   }
 
@@ -77,10 +95,12 @@ void checkBallPos(){
   if(ball.y < 19 && (ball.x >= paddlepos && ball.x <= paddlepos+PADDLE_WIDTH)){
     ball.y = 19;
     ydir = 1;
+    playTone(500, 50);
     return;
   }
 
   if(ball.y < 10 && (ball.x < paddlepos || ball.x > paddlepos+PADDLE_WIDTH)) {
+    playTone(500,500);
     ball.y = SCREEN_HEIGHT/2;
     ball.x = SCREEN_WIDTH/2;
     ydir = 1;
@@ -116,6 +136,7 @@ void setup(){
   X_CENTRE = analogRead(HOR);
   pinMode(SEL, INPUT);
   digitalWrite(SEL, HIGH);
+  pinMode(speakerPin, OUTPUT);
   // TODO: Check if joystick is depressed here at startup to use accelerometer for control instead
   // Initialize screen
   tft.initR(INITR_REDTAB);
@@ -153,6 +174,7 @@ void setup(){
     oldball.x = ball.x;
     oldball.y = ball.y;
     checkBallPos();
+    // Separate fcn: check if ball has hit a brick here...
     ball.x += xdir;
     ball.y += ydir;
     drawBall(ball, oldball);
